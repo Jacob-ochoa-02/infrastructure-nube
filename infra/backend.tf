7c186098a3dd18a0c563ce1c04721b218ac182ef
Jacob-ochoa-02 <80126810+Jacob-ochoa-02@users.noreply.github.com>
2026-03-27 22:08:20 +0000

    Modifications to deploy

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket      = "poli-terraform"
-        key         = "jol/terraform.tfstate"
-        region      = "us-east-1"
-        dynamodb_table = "terraform-locks"
-        encrypt     = true
-    }
+  backend "s3" {
+    bucket       = "poli-terraform"
+    key          = "jol/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
